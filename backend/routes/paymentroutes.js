3b86fbb558166c329aaa8dce09ff25b2b5a4d4cb
Masango-12 <hansmukete42@gmail.com>
2025-08-13 14:26:25 +0100

    Backend updated

--- backend/routes/paymentroutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { initiatePayment, getPaymentStatus } = require('../controller/paymentcontroller');
-const { validateInitiatePayment, validateTransactionId } = require('../middleware/validation');
-router.post('/initiate', validateInitiatePayment, initiatePayment);
-router.get('/status/:transactionId', validateTransactionId, getPaymentStatus);
-module.exports = router;
